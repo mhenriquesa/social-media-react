fecccee68f0e259ea32f598bfdb3a047a1706208
M. Henrique S.A <henriqueator@gmail.com>
2022-02-05 10:42:18 -0300

    Page template

--- app/Main.js
+++ app/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header';
+import Terms from './pages/Terms';
+import About from './pages/About';
 import Footer from './components/Footer';
-import About from './components/pages/About';
-import HomeGuest from './components/HomeGuest';
+import Header from './components/Header';
+import HomeGuest from './pages/HomeGuest';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function Main() {
@@ -13,6 +14,7 @@ function Main() {
       <Routes>
         <Route exact path='/' element={<HomeGuest />} />
         <Route exact path='/about-us' element={<About />} />
+        <Route exact path='/terms' element={<Terms />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- app/components/Container.js
+++ app/components/Container.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react"
 function Container(props) {
   return (
     <div className={"container py-md-5 " + (props.wide ? '' : 'container--narrow') }>
-    {/* <div className="container container--narrow py-md-5"> */}
       {props.children}
     </div>
   )
