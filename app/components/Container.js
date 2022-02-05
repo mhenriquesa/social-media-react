8ee47c09e0a739c0e1a3a5ccf3e5b64bb7054ceb
M. Henrique S.A <henriqueator@gmail.com>
2022-02-05 00:29:57 -0300

    Add a reuseble Container Component

---
+++ app/components/Container.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react"
+
+function Container(props) {
+  return (
+    <div className={"container py-md-5 " + (props.wide ? '' : 'container--narrow') }>
+    {/* <div className="container container--narrow py-md-5"> */}
+      {props.children}
+    </div>
+  )
+}
+
+export default Container
