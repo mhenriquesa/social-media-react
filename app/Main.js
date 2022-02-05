2511b88a996b7be733af028684a3bface08b0977
M. Henrique S.A <henriqueator@gmail.com>
2022-02-04 23:17:24 -0300

    Router JS; About and Terms Pages; Links on nav bars

--- app/Main.js
+++ app/Main.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/pages/About';
 import HomeGuest from './components/HomeGuest';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function Main() {
   return <>
-    <Header />
-    <HomeGuest />
-    <Footer />
+    <BrowserRouter >
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomeGuest />} />
+        <Route exact path='/about-us' element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </>
 }
 
